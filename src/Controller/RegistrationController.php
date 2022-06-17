559e6fc92c8bc34112efc9d34663e7d6129c2863
Quentin <quentin.altieri@gmail.com>
2022-06-17 17:35:51 +0200

    commit

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this -> passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @Route("/registration", name="registration")
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        $user = new User();
+
+        $form = $this -> createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+          //Encode les nouveaux mots de passes des users
+
+            $user->setPassword($this->passwordEncoder->encodePassword($user,$user->getPassword()));
+
+       $user->setRoles(['ROLE_USER']);
+
+       //Sauvegarde
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form-> createView(),
+        ]);
+    }
+}
